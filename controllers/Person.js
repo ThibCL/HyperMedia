a708692b705f035ebeb59ea9588e2b3df6530200
ThibCL <thibault.cabourg@gmail.com>
2020-05-29 02:35:52 +0200

    implementation endpoint get person by id

--- controllers/Person.js
+++ controllers/Person.js
@@ -25,7 +25,12 @@ module.exports.getPersonByID = function getPersonByID(req, res, next) {
       utils.writeJson(res, response)
     })
     .catch(function (response) {
-      utils.writeJson(res, response)
+      console.error(response)
+      if (response.error && response.statusCode) {
+        utils.writeJson(res, response.error, response.statusCode)
+      } else {
+        utils.writeJson(res, { error: "Please retry later" }, 500)
+      }
     })
 }
 
